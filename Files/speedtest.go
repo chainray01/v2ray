6de177c4210f69d154632d7c7223e7b4c039b3e4
ray.chai <ray.chai@icloud.com>
2025-10-20 14:27:24 +0800

    commit

---
+++ Files/speedtest.go
@@ -0,0 +1,37 @@
+import (
+    "context"
+    "fmt"
+	"time"
+    "github.com/xxf098/lite-proxy/web"
+)
+// see more details in ./examples
+func testPing() error {
+    ctx := context.Background()
+    link := "https://www.example.com/subscription/link"
+    opts := web.ProfileTestOptions{
+		GroupName:     "Default", 
+		SpeedTestMode: "pingonly",   //  pingonly speedonly all
+		PingMethod:    "googleping", // googleping
+		SortMethod:    "rspeed", // speed rspeed ping rping
+		Concurrency:   2,
+		TestMode:      2,
+		Subscription:  link,
+		Language:      "en",  // en cn
+		FontSize:      24,
+		Theme:         "rainbow",
+        Unique:        true,
+		Timeout:       10 * time.Second,
+		OutputMode:  0,
+	}
+    nodes, err := web.TestContext(ctx, opts, &web.EmptyMessageWriter{})
+    if err != nil {
+        return err
+    }
+    // get all ok profile
+    for _, node := range nodes {
+        if node.IsOk {
+			fmt.Println(node.Remarks)
+		}
+	}
+    return nil
+}
--- Files/speedtest.sh
+++
@@ -1,6 +0,0 @@
-#准备好所需文件
-wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
-gzip -d lite-linux-amd64.gz
-#运行 LiteSpeedTest
-chmod +x ./lite-linux-amd64
-sudo nohup ./lite-linux-amd64 --config speedconfig.json --test ../data/All_Configs_Sub.txt >speedtest.log 2>&1 &
