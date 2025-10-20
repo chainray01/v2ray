fb45e80ec569b05073ca156743798b59d9ba0adc
ray.chai <ray.chai@icloud.com>
2025-10-20 14:11:57 +0800

    commit

---
+++ Files/speedtest.sh
@@ -0,0 +1,6 @@
+#准备好所需文件
+wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
+gzip -d lite-linux-amd64.gz
+#运行 LiteSpeedTest
+chmod +x ./lite-linux-amd64
+sudo nohup ./lite-linux-amd64 --config ../data/All_Configs_Sub.txt --test subs >speedtest.log 2>&1 &
